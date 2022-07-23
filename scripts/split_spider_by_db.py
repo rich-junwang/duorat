76fd14d3f47130c2cc11f7906f332bdfd9349afa
Jun Wang <richard.junwang@gmail.com>
2022-07-22 23:49:44 -0400

    different function to process spider and squall

--- scripts/split_spider_by_db.py
+++ scripts/split_spider_by_db.py
@@ -6,12 +6,9 @@ from collections import defaultdict
 from typing import List, Dict
 
 
-def main(spider_path, duorat_path, dataset) -> None:
+def process_spider(spider_path, duorat_path) -> None:
     tables_json_path = "tables.json"
-    if dataset == "spider":
-        examples_paths = ["train_spider.json", "train_others.json", "dev.json"]
-    else:
-        examples_paths = ["train.json", "dev.json"]
+    examples_paths = ["train_spider.json", "train_others.json", "dev.json"]
 
     # 1. Produce tables.json files
     with open(os.path.join(spider_path, tables_json_path), "r") as read_fp:
@@ -42,6 +39,43 @@ def main(spider_path, duorat_path, dataset) -> None:
                 json.dump(payload_group, write_fp, indent=2)
 
 
+def process_squall(spider_path, duorat_path, dataset) -> None:
+    tables_json_path = "tables.json"
+    examples_paths = ["train.jsonl", "dev.jsonl"]
+
+    # 1. Produce tables.json files
+    with open(os.path.join(spider_path, tables_json_path), "r") as read_fp:
+        payload: List[dict] = json.load(read_fp)
+
+    grouped_payload: Dict[str, dict] = {}
+    for item in payload:
+        db_id: str = item['db_id']
+        assert db_id not in grouped_payload
+        grouped_payload[db_id] = item
+
+    for db_id, item in grouped_payload.items():
+        with open(os.path.join(duorat_path, db_id, "tables.json"), "wt") as write_fp:
+            json.dump([item], write_fp, indent=2)
+
+    # 2. Produce examples.json files
+    for examples_path in examples_paths:
+        with open(os.path.join(spider_path, examples_path), "r") as read_fp:
+            payload: List[dict] = []
+            for line in read_fp:
+                line = line.strip()
+                if line:
+                    payload.append(json.loads(line))
+
+        grouped_payload: Dict[str, List[dict]] = defaultdict(list)
+        for item in payload:
+            db_id: str = item['db_id']
+            grouped_payload[db_id].append(item)
+
+        for db_id, payload_group in grouped_payload.items():
+            with open(os.path.join(duorat_path, db_id, "examples.json"), "wt") as write_fp:
+                json.dump(payload_group, write_fp, indent=2)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default='spider', choices=["spider", "squall", "spider_aug"])
@@ -49,4 +83,10 @@ if __name__ == '__main__':
     parser.add_argument("--duorat-path", type=str, default='data/database')
     args = parser.parse_args()
 
-    main(args.spider_path, args.duorat_path, args.dataset)
+    if args.dataset == "spider":
+        process_spider(args.spider_path, args.duorat_path)
+    elif args.dataset == "squall":
+        process_squall(args.spider_path, args.duorat_path)
+    else:
+        print("unsupport format")
+        exit(1)
